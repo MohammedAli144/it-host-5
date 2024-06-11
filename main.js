c6180995f11228cbaa6deee28ef57d37a981026a
MohammedAli144 <1411mody@gmail.com>
2024-06-11 22:51:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let icon = document.getElementById('icon');
+let nav = document.getElementById('nav');
+
+icon.onclick =function ()
+    {
+        nav.style.display = 'block'
+    }
+// icon.onclick = function () 
+//     {
+//         nav.style.display = 'none'
+//     }
